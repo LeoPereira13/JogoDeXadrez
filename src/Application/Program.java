54724d892a7204a80374af91730377d316937e6a
Leo Pereira <leo.dpsouza@gmail.com>
2022-12-20 16:34:00 -0300

    Começando a implementar as peças do tabuleiro

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,10 +1,12 @@
 package Application;
 
+import boardGame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Board board = new Board(8, 8);
 	}
 
 }
--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -9,6 +9,28 @@ public class Position {
 		this.row = row;
 		this.column = column;
 	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
 	
+	
+	@Override
+	public String toString(){
+		
+		return row + ", " + column;
+	}
 	
 }
