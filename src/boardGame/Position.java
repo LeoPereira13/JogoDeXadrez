61f085275159c56ad72a1b02559338661eed9360
Leo Pereira <leo.dpsouza@gmail.com>
2022-12-20 16:09:24 -0300

    Primeira classe: posição

---
+++ src/boardGame/Position.java
@@ -0,0 +1,14 @@
+package boardGame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {		
+		this.row = row;
+		this.column = column;
+	}
+	
+	
+}
