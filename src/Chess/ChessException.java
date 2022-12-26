413adce68dbe9ef4068700341ce7f2826fee5b38
Leo Pereira <leo.dpsouza@gmail.com>
2022-12-26 17:17:56 -0300

    ChessException and ChessPosition

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,10 @@
+package Chess;
+
+public class ChessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		 super(msg);
+	}
+}
