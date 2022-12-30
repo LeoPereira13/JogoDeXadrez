64f3337374c535cfe30ae43de8e888b016c13d82
Leo Pereira <leo.dpsouza@gmail.com>
2022-12-30 13:02:39 -0300

    lógica de checkmate

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -16,7 +16,8 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while(true) {
+		
+		while(!chessMatch.getCheckMate()) {
 			try {
 				uI.clearScreen();
 				uI.printMatch(chessMatch, captured);
@@ -47,6 +48,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		uI.clearScreen();
+		uI.printMatch(chessMatch, captured);
 	}
 
 }
--- src/Application/uI.java
+++ src/Application/uI.java
@@ -61,9 +61,16 @@ public class uI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+			
+		}else {
+			System.out.println("CHECKMATE!!!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
