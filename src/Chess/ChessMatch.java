015d8e4b4b4711f244ca0f6b9cb1ecde64ed9b21
Leo Pereira <leo.dpsouza@gmail.com>
2023-01-05 09:08:29 -0300

    Melhorando tratamento da exceção

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,6 @@
 package Chess;
 
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -126,7 +126,7 @@ public class ChessMatch {
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
 			
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 			
 		}
 		
